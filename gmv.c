cc9349faeb0878bf766d9c5fa8c9c1ec15f8be53
WST <admin@jsmart.web.id>
2014-02-10 16:51:31 +0400

    Okay, adding some real functionality

--- gmv.c
+++ gmv.c
@@ -1,6 +1,12 @@
 
 #include "gmv.h"
 
+// Function to read GMV header
+void readMovieHeader(FILE *handle, MovieHeader *header) {
+	fseek(handle, 0, SEEK_SET);
+	fread(header, sizeof(MovieHeader), 1, handle);
+}
+
 PHP_FUNCTION(gmv_info);
 
 const zend_function_entry gmv_functions[] = { PHP_FE(gmv_info, 0) {0, 0, 0} };
@@ -13,11 +19,26 @@ PHP_FUNCTION(gmv_info) {
     char *filename;
     int length;
 
-    // Our return value
-    array_init(return_value);
-    add_index_long(return_value, 123, 456);
-
+    // Parsing the arguments
     if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &length)) return;
 
-    //RETURN_ZVAL(return_value, 1, 1);
+    // Trying to open the provided GMV file
+    FILE *handle = fopen(filename, "r");
+
+    // Allocating some memory for the header
+    MovieHeader *header = (MovieHeader *) malloc(sizeof(MovieHeader));
+
+    // Reading file header
+    readMovieHeader(handle, header);
+
+    // Closing the opened GMV file
+    fclose(handle);
+
+    // Initializing the return value
+    array_init(return_value);
+
+    // Appending items to the return array
+    // FIXME WARNING: there may be no ending zero in the comment!!!
+    add_index_string(return_value, 0, header->comment, 1);
+    add_index_long(return_value, 1, header->rerecords);
 }
--- gmv.h
+++ gmv.h
@@ -1,23 +1,27 @@
 
-
 #ifndef PHP_EXTGMV_H
-# define PHP_EXTGMV_H
+#define PHP_EXTGMV_H
 
 #ifdef HAVE_CONFIG_H
-# include "config.h"
+#include "config.h"
 #endif
 
 #include "php.h"
 
-/*
-  Define the extension version
-*/
 #define PHP_EXTGMV_VERSION "0.0.1"
-
-/*
-  Define an entry-point to the extension
-*/
 extern zend_module_entry gmv_module_entry;
 #define phpext_extgmv_ptr &gmv_module_entry
+
+#include <stdint.h>
+
+typedef struct {
+	uint8_t signature[15];
+	uint8_t gmv_version;
+	uint32_t rerecords;
+	uint8_t joypad_keys[2];
+    uint8_t flags;
+    uint8_t ext_flags;
+	uint8_t comment[40];
+} MovieHeader;
 
 #endif /* PHP_EXTSAMPLE_H */
