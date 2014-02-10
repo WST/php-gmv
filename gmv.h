54cc30fab4373ca14ace6a2ab915845b1f816241
WST <admin@jsmart.web.id>
2014-02-10 17:23:22 +0400

    Getting rid of ugliness

--- gmv.h
+++ gmv.h
@@ -19,8 +19,8 @@ typedef struct {
 	uint8_t gmv_version;
 	uint32_t rerecords;
 	uint8_t joypad_keys[2];
-    uint8_t flags;
-    uint8_t ext_flags;
+	uint8_t flags;
+	uint8_t ext_flags;
 	uint8_t comment[40];
 } MovieHeader;
 
